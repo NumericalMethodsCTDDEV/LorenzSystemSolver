1ceab582398ad29a9b9258ebe301dd5623eee63f
kant97 <kirant9797@gmail.com>
2018-09-12 15:46:57 +0300

    add console support

--- src/ui/console/frontend.cpp
+++ src/ui/console/frontend.cpp
@@ -1,7 +1,9 @@
 #include "../frontend.h"
 #include "config.h"
-#include "show_method_names.h"
 #include "internal_method_names.h"
+#include "show_method_names.h"
+#include "solver.h"
+#include <algorithm>
 #include <fstream>
 #include <iostream>
 
@@ -14,16 +16,35 @@ void printUsage()
     cout << "x0\ny0\nz0\nsigma\nr\nb\ndt\nt0\nt_max\nmethod_name" << endl;
 }
 
+void printAns(Answer &ans)
+{
+    if (!ans.solvedCorrect())
+    {
+        cout << ans.error_msg << endl;
+        return;
+    }
+    size_t sz =
+        std::min(std::min(std::min(ans.t.size(), ans.x.size()), ans.y.size()), ans.z.size());
+    for (size_t i = 0; i != sz; ++i)
+    {
+        cout << ans.t[i] << ": " << ans.x[i] << " " << ans.y[i] << " " << ans.z[i] << endl;
+    }
+}
+
 int showUi(int argc, char **argv)
 {
     ifstream fin;
-    if (argc > 1) {
+    if (argc > 1)
+    {
         fin.open(argv[1]);
-    } else {
+    }
+    else
+    {
         fin.open("input.txt");
     }
 
-    if (!fin.good()) {
+    if (!fin.good())
+    {
         printUsage();
         return 1;
     }
@@ -46,6 +67,9 @@ int showUi(int argc, char **argv)
     fin.close();
 
     ConfigSingleton::createConfigs(x0, y0, z0, sigma, r, b, dt, t_max, method_name.c_str());
+
+    Answer ans = solve();
+    printAns(ans);
 
     return 0;
 }
