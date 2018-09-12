aa66d0fe074a4005ed069a1e4c1bfbd41e1809eb
kant97 <kirant9797@gmail.com>
2018-09-12 18:44:08 +0300

    change pngcairo to png

--- src/ui/gnuplot_support_shared.cpp
+++ src/ui/gnuplot_support_shared.cpp
@@ -10,7 +10,7 @@ int buildChartGnuPlot()
         printf("Error opening pipe to GNU plot.\n");
         return 1;
     }
-    fprintf(gp, "set terminal pngcairo\n");
+    fprintf(gp, "set terminal png\n");
     fprintf(gp, "set terminal png size 1920,1200\n");
     fprintf(gp, "set output \"chart.png\"\n");
     fprintf(gp, "set key inside left top vertical Right noreverse enhanced autotitles box linetype -1 linewidth 1.000\n");
